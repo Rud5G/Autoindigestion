85bdf02fd032c12cc61ff2a3dacb443427e4051d
Don McCaughey <don@ablepear.com>
2013-05-22 22:25:58 -0700

    Inlined -fileLength method of NSData (ZipBrowser) category.
    It just called -length on the NSData object anyway.

--- Installer/DownloadAutoingestionPlugin/ZipBrowser/NSData+ZipBrowser.h
+++ Installer/DownloadAutoingestionPlugin/ZipBrowser/NSData+ZipBrowser.h
@@ -5,8 +5,6 @@
 
 - (NSData *)dataAtOffset:(unsigned long long)offset length:(NSUInteger)length;
 
-- (unsigned long long)fileLength;
-
 - (uint32_t)littleUnsignedIntAtOffset:(unsigned long long)offset;
 
 - (uint16_t)littleUnsignedShortAtOffset:(unsigned long long)offset;
